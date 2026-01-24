7a2e981b1e73df9dd02ef741f103d28ba95e3a4a
creativeGhalib <creativeghalib@gmail.com>
2026-01-24 17:31:41 +0600

    simple bmi calculator task

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-//
-let weight = 65;
-let hight = 6;
-let bmi = weight / (hight * hight);
-console.log(bmi);
 
+// Simple BMI Calculator
+let weight = 65;
+
+let height = 5.5;
+
+let heightInMeters = height * 0.3048;
+
+let bmi = weight / (heightInMeters * heightInMeters);
+
+console.log('Your weight: ' + weight + ' kg');
+console.log('Your height: ' + height + ' feet');
+console.log('Your BMI: ' + bmi.toFixed(1));
+
+console.log('');
+console.log('Result:');
+
+if (bmi < 18.5) {
+  console.log('You are Underweight ');
+  console.log(' Try to eat more healthy food!');
+} else if (bmi < 25) {
+  console.log('You are Normal Weight ');
+  console.log('Great job! Keep it up!');
+} else if (bmi < 30) {
+  console.log('You are Overweight ');
+  console.log('Try some exercise and eat healthy!');
+} else {
+  console.log('You are Obese ');
+  console.log('Please talk to a doctor for help!');
+}
