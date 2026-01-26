590aac839329ce791c6a5fc9afc20cc74edcf142
CreativeGhalib <creativeghalib@gmail.com>
2026-01-26 12:43:18 +0600

    slice and splice,slice ,slice: পুরনো array change করে না, শুধু copy করে। splice: পুরনো array-কে change করে (add/remove), এবং remove করা item গুলো return করে।

--- addandremove.js
+++ addandremove.js
@@ -1,31 +1,30 @@
 const friends = ['Alice', 'Bob', 'Charlie'];
-friends.push("MESBAH");
+friends.push('MESBAH');
 console.log(friends);
 const removedFriend = friends.pop();
 console.log(`removedFriend: ${removedFriend}`);
-const firstFriend = friends.shift()
+const firstFriend = friends.shift();
 console.log(friends);
 console.log(firstFriend);
-const newFriendCount = friends.unshift("MESBAH");
+const newFriendCount = friends.unshift('MESBAH');
 console.log(newFriendCount);
 console.log(friends);
-console.log(friends.indexOf("Bob"));
+console.log(friends.indexOf('Bob'));
 console.log(friends.includes('Bob'));
-length=friends[2].length;
+length = friends[2].length;
 console.log(`Lenth is ${length}`);
-let a='Hello';
+let a = 'Hello';
 console.log(a.length);
 let b = 'hello';
 // Use of uppercase and lower case
 if (a.toLowerCase() === b.toLowerCase()) {
-  console.log("value is same");
+  console.log('value is same');
 } else {
-  console.log("Value not same");
+  console.log('Value not same');
 }
-console.log(friends.indexOf("MESBAH")
-);
+console.log(friends.indexOf('MESBAH'));
 console.log(friends);
-console.log(friends.indexOf("Bob"));
+console.log(friends.indexOf('Bob'));
 let newwfrnd = friends.map(f => f.toLowerCase());
 console.log(newwfrnd);
 
@@ -37,5 +36,9 @@ console.log(joinedFriends);
 const moreFriends = ['David', 'Eve'];
 const allFriends = friends.concat(moreFriends);
 console.log(allFriends);
-
-
+// slice
+const someFriends = allFriends.slice(1, 4);
+console.log(someFriends);
+// splice
+allFriends.splice(2, 1, 'Zara');
+console.log(allFriends);
