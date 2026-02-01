cf29e7a271e4dc9ef9beaedbb572918b40a6cd89
CreativeGhalib <creativeghalib@gmail.com>
2026-02-01 12:24:02 +0600

    do while loop task

--- addandremove.js
+++ addandremove.js
@@ -343,9 +343,19 @@ Implement a countdown timer that counts down from 21 to 15. using for loop
 // }
 
 // Finding out odd or even nummber using for loop & continue.
-for (let i = 1; i < 20; i++){
-  if (i % 2 !== 0) {
-    continue;
-  }
+// for (let i = 1; i < 20; i++){
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+
+
+// Do while loop task
+let i = 0;
+do {
   console.log(i);
+  i++
 }
+while(i<10)
