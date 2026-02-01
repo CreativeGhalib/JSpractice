39b420d956e00e37e0bfea3519b520266cd100b7
CreativeGhalib <creativeghalib@gmail.com>
2026-02-01 12:09:17 +0600

    Finding out odd or even nummber using for loop & continue.

--- addandremove.js
+++ addandremove.js
@@ -329,15 +329,23 @@ Implement a countdown timer that counts down from 21 to 15. using for loop
 //     console.log(i);
 //   }
 // }
-for (let i = 1; i <= 30; i++) {
-  if (i % 5 === 0 || i%2===0) {
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 5 === 0 || i%2===0) {
 
-    if (i === 16) {
-      continue;
-    }
-    if (i === 24) {
-      break;
-    }
+//     if (i === 16) {
+//       continue;
+//     }
+//     if (i === 24) {
+//       break;
+//     }
+//   }
+//   console.log(i);
+// }
+
+// Finding out odd or even nummber using for loop & continue.
+for (let i = 1; i < 20; i++){
+  if (i % 2 !== 0) {
+    continue;
   }
   console.log(i);
 }
