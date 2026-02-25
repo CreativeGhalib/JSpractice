3b52bbd239550c0f25ecb9b7f480d30952e8f2bf
CreativeGhalib <creativeghalib@gmail.com>
2026-02-25 22:31:57 +0600

    adding newchild using dom manipulation and using alternative way (addEventlistener)

---
+++ domDe.js
@@ -0,0 +1,22 @@
+// function addName() {
+//   const inputName = document.getElementById('input-name');
+//   const newName = inputName.value;
+//   const nameContainer = document.getElementById('name-container');
+//   const newParagraph = document.createElement('p');
+//   newParagraph.innerText = newName;
+//   nameContainer.appendChild(newParagraph);
+//   inputName.value = '';
+// }
+// Now we will do the same task using addEventListener
+document.getElementById('addname-btn')
+  .addEventListener("click", function () {
+    const nameInput = document.getElementById('input-name');
+
+    const newName = nameInput.value;
+    const nameContainer = document.getElementById('name-container');
+    const newP = document.createElement('p');
+    newP.innerText = newName;
+    nameContainer.appendChild(newP);
+    nameInput.value=''
+
+})
