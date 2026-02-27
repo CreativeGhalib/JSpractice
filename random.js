62bc98eb62d3f946e60f8f57b5904369a5df4506
CreativeGhalib <creativeghalib@gmail.com>
2026-02-27 16:53:12 +0600

    learing about how arrow function works ,default behaviour of the parameters,how we can use default value in the parameter,spread operator

---
+++ random.js
@@ -0,0 +1,12 @@
+// function add(num1, num2 = 0) {
+//   const total = num1 + num2;
+//   console.log(num1, num2, total);
+
+// }
+// add(10);
+// add(10, 20);
+
+const numbers = [12, 11, 2, 3, 4, 15];
+const maxNum = Math.max(...numbers);
+console.log(...numbers)
+ 
