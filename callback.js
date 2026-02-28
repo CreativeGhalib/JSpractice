62f2fa4e273a3104f9ad159a5f961d912f7f34c3
CreativeGhalib <creativeghalib@gmail.com>
2026-02-28 23:39:28 +0600

    addtion function task performing callback function

--- callback.js
+++ callback.js
@@ -2,14 +2,24 @@
 //   .addEventListener("click", function (event) {
 
 // })
-function settleLife(name, isBcs,marriage,patri) {
-  if (isBcs) {
-    marriage(patri);
-  }
-}
-function boloKobul(patri) {
-  console.log('kobul',patri);
-}
+// function settleLife(name, isBcs,marriage,patri) {
+//   if (isBcs) {
+//     marriage(patri);
+//   }
+// }
+// function boloKobul(patri) {
+//   console.log('kobul',patri);
+// }
 
-settleLife("tomal", true, boloKobul,"nari");
+// settleLife("tomal", true, boloKobul,"nari");
+
+
+function displayNumber(num) {
+  console.log(num);
+}
+function addiTion(num1, num2) {
+  let sum = num1 + num2;
+  displayNumber(sum)
+}
+addiTion(12, 12);
 
