1c6f598a12a6736f66fb19e3aad95160c20daa87
CreativeGhalib <creativeghalib@gmail.com>
2026-03-01 11:43:14 +0600

    understanding difference between arguments,pass by reference and passby value

--- callback.js
+++ callback.js
@@ -14,12 +14,18 @@
 // settleLife("tomal", true, boloKobul,"nari");
 
 
-function displayNumber(num) {
-  console.log(num);
-}
-function addiTion(num1, num2) {
-  let sum = num1 + num2;
-  displayNumber(sum)
-}
-addiTion(12, 12);
+// function displayNumber(num) {
+//   console.log(num);
+// }
+// function addiTion(num1, num2) {
+//   let sum = num1 + num2;
+//   displayNumber(sum)
+// }
+// addiTion(12, 12);
 
+function add(num1, num2) {
+  console.log("arguments:", arguments);
+  const args = [...arguments];
+  console.log(args)
+}
+add(12, 14,11,9)
