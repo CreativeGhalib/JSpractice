8fd291a5a81fd1fa9d860cf41b2e7eb892bbf0ce
CreativeGhalib <creativeghalib@gmail.com>
2026-03-01 12:10:04 +0600

    we can do the same thing using a single line code

--- map.js
+++ map.js
@@ -7,7 +7,14 @@
 
 // we can do the same thing using map and callback function
 
+// const numbers = [1, 2, 3, 4, 5];
+// const doubleIt = (num) => num * 2;
+// const doubled = numbers.map(doubleIt);
+// console.log(doubled)
+
+
+// we can do the same thing using a single line code
+
 const numbers = [1, 2, 3, 4, 5];
-const doubleIt = (num) => num * 2;
-const doubled = numbers.map(doubleIt);
+const doubled = numbers.map(num => num * 2);
 console.log(doubled)
