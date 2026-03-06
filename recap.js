6f207655eb1d5d614df25a88983e856a9e6192d0
CreativeGhalib <creativeghalib@gmail.com>
2026-03-06 23:27:13 +0600

    practicing fetch function

--- recap.js
+++ recap.js
@@ -12,3 +12,23 @@ function showSpinner() {
 function hideSpinner() {
   spinner.classList.add('hidden');
 }
+
+// fetch function
+async function loadIssues() {
+  try {
+    showSpinner();
+    grid.innerHTML = ''; //আগের data clear
+
+    const res = await fetch('./data/issues.json');
+    const data = await res.json();
+
+    // এখানে render function কল হবে
+    // renderIssues(data);
+  } catch (err) {
+    grid.innerHTML = `<p class="text-red-600">Failed to load</p>`;
+  } finally {
+    hideSpinner(); // success বা error যাই হোক spinner বন্ধ হবে
+  }
+}
+
+loadIssues();
